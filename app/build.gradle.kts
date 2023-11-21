f1df3fd2660fabd6ab3455143e6b9d55740599b6
Darrell Lim <limdarr28@gmail.com>
2023-11-20 22:14:59 -0500

    Removed MVVM to Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.lifecycle:lifecycle-view-model- ktx:2.6.2")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
